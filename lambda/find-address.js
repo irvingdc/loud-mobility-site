92e5e8aa8275701c1d17994b632e82bdbb47ce5d
irvingdc <irvingedc@gmail.com>
2021-08-25 14:12:08 -0500

    added netlify function to query the address API

--- lambda/find-address.js
+++ lambda/find-address.js
@@ -1,11 +1,11 @@
 const fetch = require('node-fetch')
 
 let API_KEY = "ak_ksdk4bsyiEE6rl63vzXtJLk5kXu4E";
-const API_ENDPOINT = `https://api.ideal-postcodes.co.uk/v1/autocomplete/addresses?api_key=${API_KEY}&query=${query}`
 
 exports.handler = async (event, context) => {
   let response
   try {
+    const API_ENDPOINT = `https://api.ideal-postcodes.co.uk/v1/autocomplete/addresses?api_key=${API_KEY}&query=${event.queryStringParameters.query}`
     response = await fetch(API_ENDPOINT)
     // handle response
   } catch (err) {
